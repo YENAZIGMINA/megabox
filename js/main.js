728bb2f46068d4edf082bacab086fc68d1d64e3a
lee_yena <leeyena4724@naver.com>
2023-05-24 19:46:50 +0900

    5/24

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* ✔스와이퍼 사이트 - Initialize Swiper 복붙 */
+// #banner - .slider 
+const swiper = new Swiper('.slider', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
