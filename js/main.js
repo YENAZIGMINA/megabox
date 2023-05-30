d982305cd49614ef55033a5188e0b1d0c99b8fc1
lee_yena <leeyena4724@naver.com>
2023-05-30 18:00:32 +0900

    5/30

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* ✔스와이퍼 사이트 - Initialize Swiper 복붙 */
-// #banner - .slider 
+// 💛#banner - .slider 
 const swiper = new Swiper('.slider', {
     // Optional parameters
     loop: true,
@@ -15,4 +15,28 @@ const swiper = new Swiper('.slider', {
       prevEl: '.swiper-button-prev',
     },
   
+  });
+
+
+  // 💛movie
+  // Initialize Swiper 복붙
+  const swiper2 = new Swiper('.swiper-container2', {
+    loop: true,
+    slidesPerView: 4, //화면에 4개 보이기
+  
+  });
+
+  //movie tab menu
+  let movBtn = $('.movie_title ul li');
+  let movCont = $('.movie_chart>div');
+
+  movCont.hide().eq(0).show(); //스와이퍼 첫번째 슬라이드만 보이기
+  movBtn.click(function(e){
+    e.preventDefault();
+    let target = $(this)
+    let index = target.index(); //클릭한 li의 index 번호추출
+    console.log(index)
+    movCont.hide().eq(index).show(); //모두 지우고, 인덱스 번호와 동등한 div 보임
+    movBtn.removeClass("active"); //모든 li에 active 지우기
+    target.addClass("active"); //클릭한 li에 active 추가
   });
